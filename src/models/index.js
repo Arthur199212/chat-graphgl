26a8098245bae308b7307fab4cef8ee3309815c5
Arthur Rubchenko <2146596@mail.ru>
2019-12-19 10:11:38 +0300

    chore: clean up

--- src/models/index.js
+++
@@ -1,3 +0,0 @@
-export { default as User } from './user'
-export { default as Chat } from './chat'
-export { default as Message } from './message'
--- src/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import user from './user'
-import chat from './chat'
-
-export default [
-  user,
-  chat
-]
